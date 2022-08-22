db3111d71c653663c6c6d52e05e11655937e9521
Florent Hunkeler <florent.hunkeler@open-groupe.com>
2022-08-22 09:52:50 +0200

    fix

--- .history/src/server/router/item_20220819163315.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../db/client"
-import { createRouter } from "./context"
-
-export const itemRouter = createRouter()
-  .query('all', {
-    async resolve() {
-      return prisma.item.findMany()
-    },
-  })
